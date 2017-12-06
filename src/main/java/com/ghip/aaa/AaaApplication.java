c6fd80ab1b6c3b9d364380599c0e708cd169baab
PedroJ <jfpM2m!55>
2017-12-06 18:01:09 +0000

    Using security filters

--- src/main/java/com/ghip/aaa/AaaApplication.java
+++ src/main/java/com/ghip/aaa/AaaApplication.java
@@ -2,11 +2,18 @@ package com.ghip.aaa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class AaaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AaaApplication.class, args);
+	}
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
