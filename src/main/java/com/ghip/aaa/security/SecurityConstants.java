7275720e850296b57f98cc8b22903c9211a44017
pedro.joao@gmail.com <jfpgithub1>
2017-12-16 23:44:38 +0000

    extend validity to 30 mins

--- src/main/java/com/ghip/aaa/security/SecurityConstants.java
+++ src/main/java/com/ghip/aaa/security/SecurityConstants.java
@@ -2,7 +2,7 @@ package com.ghip.aaa.security;
 
 public class SecurityConstants {
     public static final String SECRET = "SecretKeyToGenJWTs";
-    public static final long EXPIRATION_TIME = 864_000_000; // 10 days
+    public static final long EXPIRATION_TIME = 1800000L; // 10 days
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String TOKEN_GEN_URL = "/token/**";
